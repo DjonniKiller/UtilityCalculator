7c28815acb5d242c3d4405fb9dece7b964c7e1d6
Evgenij Ryabko <jeniaryanko@mail.ru>
2022-12-07 14:58:02 +0300

    Version_0.3

--- script.js
+++ script.js
@@ -9,8 +9,26 @@
 // Свет -> Сперва нужно вычесть из ТЗС - ПЗС и получить кол-во киловат. Далее если киловаты превышают 250, то в таком случае
 // необходимо от килловат вычесть 250, полученное значение умножить на тариф1. Далее  250 необходимо умножить на тариф2. Оба итоговых значения сложить.
 // Тарифы можешь ставить произвольные.
+function getDiff(Prev, Curr){
+    if (parseInt(Prev)!=Number() || parseInt(Curr)!=Number)
+        throw 'Input includes incorrect symbols!'
+    else
+        return Curr - Prev; 
+}
 
-function DoSomeFuckingMagic(){
-    alert('Fuck you');
+
+function CalculateResult(){
+    let result = document.getElementById('Result');
+
+    try{
+        let LightDiff = getDiff(document.getElementById("PMV_Light").value, document.getElementById("CMV_Light").value);
+        let GasDiff = getDiff(document.getElementById('PMV_Gas').value, document.getElementById('CMV_Gas').value);
+        let WaterDiff = getDiff(document.getElementById('PMV_Water').value, document.getElementById('CMV_Water').value);
+
+        
+        alert('Свет '+LightDiff+'\nГаз '+GasDiff+'\nВода '+WaterDiff);  
+    }catch(exception){
+        alert(exception);
+    }  
 }
 
