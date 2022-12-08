3a18373210f14c1b4415688638900367949c4e5c
Evgenij Ryabko <jeniaryanko@mail.ru>
2022-12-08 15:20:16 +0300

    Version_0.5

--- script.js
+++ script.js
@@ -10,23 +10,32 @@
 // необходимо от килловат вычесть 250, полученное значение умножить на тариф1. Далее  250 необходимо умножить на тариф2. Оба итоговых значения сложить.
 // Тарифы можешь ставить произвольные.
 function getDiff(Prev, Curr){
-    if (parseInt(Prev)!=Number() || parseInt(Curr)!=Number)
+    if (isNaN(Prev) || isNaN(Curr))
         throw 'Input includes incorrect symbols!'
     else
-        return Curr - Prev; 
+        return parseInt(Curr) - parseInt(Prev); 
 }
 
 
 function CalculateResult(){
     let result = document.getElementById('Result');
+    let lightTariff = document.getElementById('Light').value;
+    let waterTariff = document.getElementById('Water').value;
+    let lightLargeTariff = document.getElementById('LightLarge').value;
+    let gasTariff = document.getElementById('Gas').value;
 
     try{
-        let LightDiff = getDiff(document.getElementById("PMV_Light").value, document.getElementById("CMV_Light").value);
-        let GasDiff = getDiff(document.getElementById('PMV_Gas').value, document.getElementById('CMV_Gas').value);
-        let WaterDiff = getDiff(document.getElementById('PMV_Water').value, document.getElementById('CMV_Water').value);
+        let lightDiff = getDiff(document.getElementById('PMV_Light').value, document.getElementById('CMV_Light').value);
+        let gasDiff = getDiff(document.getElementById('PMV_Gas').value, document.getElementById('CMV_Gas').value);
+        let waterDiff = getDiff(document.getElementById('PMV_Water').value, document.getElementById('CMV_Water').value);
 
+        if (lightDiff > 250)
+            lightCost = (250 * lightTariff) + (lightDiff - 250) * lightLargeTariff;
+        else
+            lightCost = lightDiff * lightTariff;
         
-        alert('Свет '+LightDiff+'\nГаз '+GasDiff+'\nВода '+WaterDiff);  
+        
+        result.textContent = `Счет за свет: ${lightCost} руб.\nСчет за газ: ${gasDiff * gasTariff} руб.\nСчет за воду: ${waterDiff * waterTariff} руб.`;
     }catch(exception){
         alert(exception);
     }  
